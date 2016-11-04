9eb80bbd59187227b2dfb130afaba258731a8558
alvarojose1 <alvinho42@gmail.com>
2016-11-04 15:55:52 -0200

    pacote domain e reserva

--- src/java/domain/Usuario.java
+++ src/java/domain/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package banco;
+package domain;
 
 import static com.sun.xml.bind.v2.schemagen.Util.equal;
 
